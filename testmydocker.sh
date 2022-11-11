f18685059a3f068a93bc6efd4efa570e6684561f
Furyan Naor <furynaor@gmail.com>
2022-11-11 18:32:47 +0200

    fix code synax of testing

--- testmydocker.sh
+++ testmydocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 # -x add debuging | -e exit test when error
 URL='localhost:7007'
-RESULT=`curl -s -I %{URL} | grep HTTP | awk {'print $2'}`
+RESULT=`curl -s -I ${URL} | grep HTTP | awk {'print $2'}`
 echo $RESULT
 if [[ $RESULT -eq 200 ]]
 then
