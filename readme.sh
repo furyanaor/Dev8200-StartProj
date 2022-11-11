d2f8a4144cb92b9927ef96f6cf0e398114a20bb2
FuryaNaor <furyanaor@gmail.com>
2022-11-11 19:45:25 +0200

    Update readme.sh

--- readme.sh
+++ readme.sh
@@ -4,5 +4,5 @@
 docker-compose build
 docker-compose up
 
-##to stop the created container using the command docker-compose down..........סץץץץץ
+##to stop the created container using the command docker-compose down..........סץRץץץץ
 #docker-compose down
